0a2a09d34d8916cdf21ce6acf94de63e3e403225
Marianne <mariannehaguiar@gmail.com>
2022-10-30 02:11:47 -0300

    validations to pokemon

---
+++ app/models/pokemon.rb
@@ -0,0 +1,10 @@
+class Pokemon < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :base_experience, presence: true
+
+
+end
+
