9b45a2c7c0d89987576dbbd21456ed0b1f11ed82
Marianne <mariannehaguiar@gmail.com>
2022-10-29 23:41:09 -0300

    adding user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+
   let(:user) { FactoryBot.create(:user) }
+
+  describe "associations" do
+    it { should have_many(:pokemons) }
+    it { should have_many(:transactions) }
+  end
 
   it "should have an valid email" do
     expect(user.email).to be_truthy
   end
+
+  context "when selling" do
+    it "should have a valid balance" do
+      expect(user.balance).to be_truthy
+    end
+  end
+
 end
