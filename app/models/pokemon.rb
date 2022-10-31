5fc46ace60b60983ccb8e0e230038307dd0cfb34
Marianne <mariannehaguiar@gmail.com>
2022-10-31 06:33:56 -0300

    adding optional true to associated model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :name, presence: true
   validates :price, presence: true
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :pokemon
 
   OPERATIONS = {
